e346a6948edb089ed086dc853f5025817cc6612e
Anil Nakum <anilnakum0842@gmail.com>
2024-10-04 14:53:16 +0530

    Write Test Cases For Calculator

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
   });
 
   it('should create the app', () => {
@@ -20,16 +13,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tdd-sting-calculator-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('tdd-sting-calculator-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tdd-sting-calculator-app');
-  });
 });
--- src/app/string-calculator.service.spec.ts
+++ src/app/string-calculator.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { StringCalculatorService } from './string-calculator.service';
 
 describe('StringCalculatorService', () => {
@@ -10,7 +9,47 @@ describe('StringCalculatorService', () => {
     service = TestBed.inject(StringCalculatorService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should return 0 for an empty string', () => {
+    expect(service.add('')).toBe(0);
+  });
+
+  it('should return the number when there is one number', () => {
+    expect(service.add('1')).toBe(1);
+  });
+
+  it('should return the sum of two numbers', () => {
+    expect(service.add('1,5')).toBe(6);
+  });
+
+  it('should handle new lines between numbers', () => {
+    expect(service.add('1\\n2,3')).toBe(6);
+  });
+
+  it('should handle a custom delimiter', () => {
+    expect(service.add('//;\\n1;2')).toBe(3);
+  });
+
+  it('should throw an error for negative numbers', () => {
+    expect(() => service.add('1,-2')).toThrowError('Negative numbers not allowed: -2');
+  });
+
+  it('should throw an error for multiple negative numbers', () => {
+    expect(() => service.add('1,-2,-3')).toThrowError('Negative numbers not allowed: -2, -3');
+  });
+
+  it("should return 2 when input is '2,1001'", () => {
+    expect(() => service.add("2,1001")).toEqual(2);
+  });
+
+  it("should return 6 when input is '//[***]\n1***2***3'", () => {
+    expect(() => service.add("//[***]\n1***2***3")).toEqual(6);
+  });
+
+  it("should return 6 when input is '//[*][%]\n1*2%3'", () => {
+    expect(() => service.add("//[*][%]\n1*2%3")).toEqual(6);
+  });
+
+  it("should return 6 when input is '//[***][%%%]\n1***2%%%3'", () => {
+    expect(() => service.add("//[***][%%%]\n1***2%%%3")).toEqual(6);
   });
 });
